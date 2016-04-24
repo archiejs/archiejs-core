cc178180785424a0423e4dfe8971845c226ac402
Naval <navalnovel@gmail.com>
2016-04-24 20:47:17 +0530

    fixes related to loading the application

--- test/one-module-tests/modules/a-type2/index.js
+++ test/one-module-tests/modules/a-type2/index.js
@@ -1,18 +1,7 @@
 'use strict';
 
-var chai = require('chai');
-var expect = chai.expect;
-var should = chai.should();
-var resolve = require('path').resolve;
-
-chai.use(require('chai-things'));
-
-var Archie= require('./../../');
-
 var A = module.exports = function setup(options, imports, registerFn) {
-  console.log("Setup A");
-  console.log(options);
-  console.log(imports);
+  console.log(__dirname);
   registerFn(null, {
     "A": this
   });
--- test/one-module-tests/tests.js
+++ test/one-module-tests/tests.js
@@ -7,9 +7,7 @@ var should = chai.should();
 var resolve = require('path').resolve;
 var basedir = resolve(__dirname);
 
-chai.use(require('chai-things'));
-
-var Archie= require('./../../');
+var Archie = require('./../../');
 
 var deptrees = require( resolve(basedir, "deptrees.json") );
 
@@ -27,33 +25,35 @@ describe('Archiejs Dependency Manager Testcases:', function(){
   it('tests creating an app with one type 1 module', function(done) {
     var tree = Archie.resolveConfig(deptrees["type1"], basedir);
     assertTree(tree);
-    console.log(tree);
     Archie.createApp(tree, function(err, archie) {
-      console.log("App is ready");
-      console.log(archie);
-      assertTree(archie.config);
-      console.log(archie.services.A.whoAmI());
+      if (err) {
+        throw err;
+      }
+      assert.isNotNull(archie.services.A);
       done();
     });
   });
 
   it('tests creating an app with one type 2 module', function(done) {
     var tree = Archie.resolveConfig(deptrees["type2"], basedir);
-    console.log(tree);
-
+    assertTree(tree);
     Archie.createApp(tree, function(err, archie) {
-      console.log("App is ready");
-      console.log(archie.services.A.whoAmI());
+      if (err) {
+        throw err;
+      }
+      assert.isNotNull(archie.services.A);
       done();
     });
   });
   
   it('tests creating an app with one type 3 module', function(done) {
     var tree = Archie.resolveConfig(deptrees["type3"], basedir);
-
+    assertTree(tree);
     Archie.createApp(tree, function(err, archie) {
-      console.log("App is ready");
-      console.log(archie.services.A.whoAmI());
+      if (err) {
+        throw err;
+      }
+      assert.isNotNull(archie.services.A);
       done();
     });
   });
