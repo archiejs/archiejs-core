4bed11e71708b0a5570713c248444b62f97db6d1
Naval <navalnovel@gmail.com>
2016-04-24 19:52:01 +0530

    fixes in resolveConfig

--- test/one-module-tests/modules/a-type1/index.js
+++ test/one-module-tests/modules/a-type1/index.js
@@ -5,14 +5,12 @@ var expect = chai.expect;
 var should = chai.should();
 var resolve = require('path').resolve;
 
-chai.use(require('chai-things'));
-
-var Archie= require('./../../');
-
 var A = module.exports = function setup(options, imports, registerFn) {
   console.log("Setup A");
-  console.log(options);
-  console.log(imports);
+  
+  assert.isNull(options);
+  assert.isNull(imports);
+
   registerFn(null, {
     "A": this
   });
--- test/one-module-tests/tests.js
+++ test/one-module-tests/tests.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var chai = require('chai');
+var assert = chai.assert;
 var expect = chai.expect;
 var should = chai.should();
 var resolve = require('path').resolve;
@@ -12,14 +13,25 @@ var Archie= require('./../../');
 
 var deptrees = require( resolve(basedir, "deptrees.json") );
 
+function assertTree(tree){
+  expect(tree.length).to.equal(1);
+  var modA = tree[0];
+  assert.isNotNull(modA.setup);
+  assert.isNotNull(modA.packagePath)
+  expect(modA.provides).to.deep.equal(['A']);
+  expect(modA.consumes).to.deep.equal([]);
+}
+
 describe('Archiejs Dependency Manager Testcases:', function(){
 
   it('tests creating an app with one type 1 module', function(done) {
     var tree = Archie.resolveConfig(deptrees["type1"], basedir);
+    assertTree(tree);
     console.log(tree);
-
     Archie.createApp(tree, function(err, archie) {
       console.log("App is ready");
+      console.log(archie);
+      assertTree(archie.config);
       console.log(archie.services.A.whoAmI());
       done();
     });
@@ -38,7 +50,6 @@ describe('Archiejs Dependency Manager Testcases:', function(){
   
   it('tests creating an app with one type 3 module', function(done) {
     var tree = Archie.resolveConfig(deptrees["type3"], basedir);
-    console.log(tree);
 
     Archie.createApp(tree, function(err, archie) {
       console.log("App is ready");
